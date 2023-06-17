c3d3100cfdb17ee9d3de0c09e0489786e35afdee
Kenzma <Ogunwalekehinde52@gmail.com>
2023-06-16 19:15:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Prints the alphabet.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putcher('\n');
+	return (0);
+}
